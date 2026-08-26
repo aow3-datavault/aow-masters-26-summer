26d42421d444346f90e2ab5e5f19399d1633ef66
v1p3rnik <36699441+v1p3rnik@users.noreply.github.com>
2026-08-26 10:55:02 +0300

    Add scheduled tournament streams

--- data.js
+++ data.js
@@ -19,13 +19,13 @@ const tournamentData = {
   ].map(([name, clan, faction]) => ({ name, clan, faction })),
   rounds: [
     { title: '1/16 финала', format: 'BO1', matches: [
-      [0, 1, 'https://twitch.tv/', 'Stream One'], [2, 3, 'https://twitch.tv/', 'Stream Two'],
-      [4, 5, 'https://youtube.com/', 'Stream Three'], [6, 7, 'https://twitch.tv/', 'Stream Four'],
+      [0, 1, 'https://twitch.tv/', 'Stream One'], [2, 3, 'https://www.youtube.com/live/Aw7cqsHh4jQ?si=ekK8VR0wev6x1NLO', 'White Pigeon vs VT_TaptapBoi C AOW3 MASTERS TOURNAMENT'],
+      [4, 5, 'https://youtube.com/', 'Stream Three'], [6, 7, 'https://www.youtube.com/live/5gf9opwOKsA?si=Taid-F8OB9JwEo5_', "ART OF WAR MASTERS '26 SUMMER | Yamitoo12 R vs General-Hassan .AS7 | LIVE"],
       [8, 9, 'https://youtube.com/', 'Stream Five'], [10, 11, 'https://twitch.tv/', 'Stream Six'],
-      [12, 13, 'https://youtube.com/', 'Stream Seven'], [14, 15, 'https://twitch.tv/', 'Stream Eight'],
+      [12, 13, 'https://www.youtube.com/live/H77cRTTNz14?si=P0QaHe_eILJpE70R', 'Monirul Vs Mosina AOW3 MASTERS TOURNAMENT'], [14, 15, 'https://twitch.tv/', 'Stream Eight'],
       [16, 17, 'https://youtube.com/', 'Stream Nine'], [18, 19, 'https://twitch.tv/', 'Stream Ten'],
-      [20, 21, 'https://youtube.com/', 'Stream Eleven'], [22, 23, 'https://twitch.tv/', 'Stream Twelve'],
-      [24, 25, 'https://youtube.com/', 'Stream Thirteen'], [26, 27, 'https://www.youtube.com/live/uenpsGpp-C8?si=tDxGAhSbgHzCS6LI', 'DEVIL_MAY_CRY VS Hexblood !!!! AOW3 MASTERS TOURNAMENT'],
+      [20, 21, 'https://youtube.com/', 'Stream Eleven'], [22, 23, 'https://www.youtube.com/live/JA-cf4V2Z8w?si=puyJUc9Jvqc3Tjnd', "!'DOOM-BRINGER'! vs ALTAIR_SON_OF_DARK AOW3 MASTERS TOURNAMENT"],
+      [24, 25, 'https://www.youtube.com/live/vAg12-ZlgSc?si=aQqbUn4un12-8PQ1', "ART OF WAR MASTERS '26 SUMMER | Condantoica vs Vincenzo_1999 | LIVE"], [26, 27, 'https://www.youtube.com/live/uenpsGpp-C8?si=tDxGAhSbgHzCS6LI', 'DEVIL_MAY_CRY VS Hexblood !!!! AOW3 MASTERS TOURNAMENT'],
       [28, 29, 'https://youtube.com/', 'Stream Fifteen'], [30, 31, 'https://twitch.tv/', 'Stream Sixteen']
     ] },
     { title: '1/8 финала', format: 'BO1', matches: Array(8).fill(null) },

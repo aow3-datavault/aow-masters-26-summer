d97fd02d5b1cbeadafeb2aa7aae083b190292866
v1p3rnik <36699441+v1p3rnik@users.noreply.github.com>
2026-08-29 12:20:44 +0300

    Update M13 disqualification bracket

--- data.js
+++ data.js
@@ -25,10 +25,10 @@ const tournamentData = {
       [12, 13, 'https://www.youtube.com/live/H77cRTTNz14?si=P0QaHe_eILJpE70R', 'Monirul Vs PiscesBrain YT AOW3 MASTERS TOURNAMENT', { scores: [1, 0], eliminated: [false, true] }], [14, 15, 'https://www.youtube.com/live/isMbM1-cIu0?si=AQLMTkwNYUVOKq7F', 'kinghall VS Tairitsu AOW3 MASTERS TOURNAMENT', { scores: [1, 0], eliminated: [false, true], status: 'technicalLoss' }],
       [16, 17, 'https://www.youtube.com/live/5EaiA5dYR4g?si=lROz9gJnWgwwG2Sb', '!VOlCAN! VS colver_1672142468 AOW3 MASTERS TOURNAMENT', { scores: [0, 1], eliminated: [true, false] }], [18, 19, 'https://www.youtube.com/live/gOk8OfqL8DA?si=npERgeN2iA3CYAQu', 'Mosina VS Guten tag AOW3 MASTERS TOURNAMENT', { scores: [0, 1], eliminated: [true, false] }],
       [20, 21, 'https://www.youtube.com/live/ui0RwxQ7ymA?si=Vv3ASKTZTQtCkcWO', '_D.A.R.K_HUNTER VS VOIN--01 AOW3 MASTERS TOURNAMENT', { scores: [1, 0], eliminated: [false, true] }], [22, 23, 'https://www.youtube.com/live/JA-cf4V2Z8w?si=puyJUc9Jvqc3Tjnd', "!'DOOM-BRINGER'! vs ALTAIR_SON_OF_DARK AOW3 MASTERS TOURNAMENT", { scores: [0, 1], eliminated: [true, false] }],
-      [24, 25, 'https://youtu.be/vY8wXG8bgy4', 'Vincenzo_1999 VS condantoico AOW3 MASTERS TOURNAMENT', { scores: [0, 1], eliminated: [true, false] }], [26, 27, 'https://www.youtube.com/live/uenpsGpp-C8?si=tDxGAhSbgHzCS6LI', 'DEVIL_MAY_CRY VS Hexblood !!!! AOW3 MASTERS TOURNAMENT', { scores: [1, 0], eliminated: [false, true] }],
+      [24, 25, 'https://youtu.be/vY8wXG8bgy4', 'Vincenzo_1999 VS condantoico AOW3 MASTERS TOURNAMENT', { scores: [1, 0], eliminated: [false, true] }], [26, 27, 'https://www.youtube.com/live/uenpsGpp-C8?si=tDxGAhSbgHzCS6LI', 'DEVIL_MAY_CRY VS Hexblood !!!! AOW3 MASTERS TOURNAMENT', { scores: [1, 0], eliminated: [false, true] }],
       [28, 29, 'https://www.youtube.com/live/9UTym9at8CU?si=ERuiwkztnjf4dMXu', '-_- pathmaker Vs DuskWell-Shark AOW3 MASTERS TOURNAMENT', { scores: [0, 1], eliminated: [true, false] }], [30, 31, 'https://www.youtube.com/live/x0Jg_VBzZHY?si=_eoXAcZAhDp2zabi', "ART OF WAR MASTERS '26 SUMMER | For A Good World vs ACCPHU.RELAXWITHME | LIVE", { scores: [0, 1], eliminated: [true, false] }]
     ] },
-    { title: '1/8 финала', format: 'BO1', matches: [[0, 3, null, null, { scores: [0, 0] }], [4, 7, null, null, { scores: [0, 0] }], [8, 11, null, null, { scores: [0, 0] }], [12, 14, null, null, { scores: [0, 0] }], [17, 19, null, null, { scores: [0, 0] }], [20, 23, null, null, { scores: [0, 0] }], [25, 26, null, null, { scores: [0, 0] }], [29, 31, null, null, { scores: [0, 0] }]] },
+    { title: '1/8 финала', format: 'BO1', matches: [[0, 3, null, null, { scores: [0, 0] }], [4, 7, null, null, { scores: [0, 0] }], [8, 11, null, null, { scores: [0, 0] }], [12, 14, null, null, { scores: [0, 0] }], [17, 19, null, null, { scores: [0, 0] }], [20, 23, null, null, { scores: [0, 0] }], [31, 26, null, null, { scores: [0, 0] }], [29, 24, null, null, { scores: [0, 0] }]] },
     { title: '1/4 финала', format: 'BO1', matches: Array(4).fill(null) },
     { title: 'Полуфинал', format: 'BO3', matches: Array(2).fill(null) },
     { title: 'Финал', format: 'BO3', matches: [null] }
@@ -44,10 +44,10 @@ const tournamentData = {
     ['KTO_ESLI_NE_MI', 'Mosina'], ['Russian princes', 'colver_1672142468'],
     ['Tianya Alliance', 'Tairitsu'],
     ['-!-SUNRISE-!-', '!VOlCAN!', 1], ['BENZIN', 'DAUD_77', 1],
-    ['-BATKEN-', 'Guten tag', 1], ['FREEDOM.113', 'Vincenzo_1999'],
+    ['-BATKEN-', 'Guten tag', 1], ['FREEDOM.113', 'Vincenzo_1999', 1],
     ['RUSSIAN STEEL.', '_D.A.R.K_HUNTER'], ['CAOTHU-!-CAOCAOTHU', 'ACCPHU.RELAXWITHME'],
     ['_THE_KONFA_', 'ALTAIR_SON_OF_DARK', 1], ['W.O.R.L.D.C.O.R.P.', '-_- pathmaker'],
-    ['sakasi997', 'condantoico', 1], ['-True Freedom-', 'Yamito012 R'],
+    ['sakasi997', 'condantoico'], ['-True Freedom-', 'Yamito012 R'],
     ['_RaKi-Na-KoNfE_', 'DEVIL_MAY_CRY'],
     ['-EAGLES-', 'DuskWell-Shark'], ['PARNI IZ STALI', 'VOIN--01', 1],
     ['HELLAS ELITE', 'for a good world', 1], ['Crescent Clan', 'GraveDigger!!!']
